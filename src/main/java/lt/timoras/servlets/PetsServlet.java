9e6d94b945eecab3ec8b68ef07e6757354d17cd5
tomasz <tomasz@noemail.net>
2013-07-03 07:45:53 +0300

    thymeleaf

--- src/main/java/lt/timoras/servlets/PetsServlet.java
+++ src/main/java/lt/timoras/servlets/PetsServlet.java
@@ -1,17 +1,60 @@
 package lt.timoras.servlets;
 
-import com.google.common.collect.Lists;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import com.google.common.collect.Lists;
 
 public class PetsServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("pets", Lists.newArrayList("dog", "cat", "goldem fish", "hamster"));
-        req.getRequestDispatcher("/thymeleaf/pets").forward(req, resp);
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        resolver.setTemplateMode("XHTML");
+        resolver.setPrefix("/WEB-INF/templates/");
+        resolver.setSuffix(".html");
+        resolver.setCacheable(true);
+        resolver.setCacheTTLMs(60000L);
+        resolver.setCharacterEncoding("utf-8");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("pets", Lists.newArrayList("dog", "cat", "goldem fish", "hamster"));
+        doService(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doService(request, response);
+    }
+
+    protected void doService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+
+        WebContext ctx = new WebContext(request, response, getServletContext(), request.getLocale());
+        String result = engine.process("pets", ctx);
+
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.println(result);
+        } finally {
+            out.close();
+        }
     }
 }
